d1d36f1f004573e99f07dda70b2453b551cb882a
PrintfDead <ripprintf@gmail.com>
2022-08-29 05:23:32 +0000

    New Install, Update package.json, Add nodemon.

--- install.py
+++ install.py
@@ -1,19 +1,19 @@
 import requests, subprocess, getch, time, sys
 from colorama import Fore
 try:
-    print(f"{Fore.BLUE}==> {Fore.WHITE}Inicializando package.json")
-    time.sleep(2)
-    subprocess.run(["npm", "init", "-y"])
     time.sleep(3)
     print(f"{Fore.BLUE}==> {Fore.WHITE}Instalando paquetes")
     time.sleep(2)
-    subprocess.run(["npm", "install", "typescript", "tslint", "-D"])
+    subprocess.run(["npm", "install", "typescript", "tslint", "eslint", "nodemon", "ts-node", "-D"])
     subprocess.run(["npm", "install", "discord.js", "mysql", "@types/mysql"])
     time.sleep(3)
     print(f"{Fore.BLUE}==> {Fore.WHITE}Paquetes instalados")
     time.sleep(2)
-    print(f"{Fore.BLUE}==> {Fore.WHITE}Mostrando rama de carpetas")
+    print(f"{Fore.BLUE}==> {Fore.WHITE}Transpilando Codigo.")
     time.sleep(2)
+    subprocess.run(["tsc"])
+    print(f"{Fore.BLUE}==> {Fore.WHITE}Mostrando rama de carpetas")
+    time.sleep(3)
     if sys.platform == "windows":
         subprocess.run(["tree", "-Exclude", "node_modules"])
     else:
